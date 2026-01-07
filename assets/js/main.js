a8f5ba6fea3e5a0fd5fc400bd95f491699f98107
hwang seungwon <hsw_4935@naver.com>
2026-01-07 10:40:13 +0900

    modify

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,20 @@ import { dragScrollFn } from './dragScroll.js';
 import { sectionMotion } from './ui.js';
 import { homeVisual } from './home.js';
 
-const config = {
-    debug : true
-};
+// const config = {
+//     debug : true
+// };
 
-document.addEventListener('DOMContentLoaded', () => {
-    uiFn();
-    dragScrollFn();
-    sectionMotion();
-    homeVisual();
+// document.addEventListener('DOMContentLoaded', () => {
+//     uiFn();
+//     dragScrollFn();
+//     sectionMotion();
+//     homeVisual();
     
-    console.log('App initialized');
-});
+//     console.log('App initialized');
+// });
+
+uiFn();
+dragScrollFn();
+sectionMotion();
+homeVisual();
